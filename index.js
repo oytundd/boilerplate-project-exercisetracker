8b7b9edb7ed7c0a67561d816573df3268505d4d4
oytundd <oytundd@gmail.com>
2024-02-08 18:02:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true })) 
+const userArr = [];
 
+app.post('/api/users', (req,res) => {
+  const username = req.body.username;
+  const id = userArr.length;
+  userArr.push({username:username, _id:id});
+});
+
+app.get('/api/users', (req,res) => {
+  res.json(userArr);
+});
 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
